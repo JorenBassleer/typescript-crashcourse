d3eeab982920cfdb819fd649d0b80a2571b57491
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-02 19:51:57 +0200

    added more  appliance calls

--- src/api/appliance.ts
+++ src/api/appliance.ts
@@ -1,8 +1,24 @@
 import type Appliance from '@/types/Appliance';
-import axios from 'axios';
+import axios, { type AxiosResponse } from 'axios';
 
 
 export const getAppliances = async () : Promise<Appliance[]> => {
   const { data } =  await axios.get<Appliance[]>(`${import.meta.env.VITE_API_URL}/appliance`);
   return data;
-}
+};
+export const getAppliance = async (_id: string): Promise<Appliance> => {
+  const { data } = await axios.get<Appliance>(`${import.meta.env.VITE_API_URL}/appliance/${_id}`);
+  return data;
+};
+export const createAppliance = async (appliance: Appliance): Promise<Appliance> => {
+  const { data } = await axios.post<Appliance>(`${import.meta.env.VITE_API_URL}/appliance`, appliance);
+  return data;
+};
+export const updateAppliance = async (appliance: Appliance, _id: string): Promise<Appliance> => {
+  const { data } = await axios.put<Appliance>(`${import.meta.env.VITE_API_URL}/appliance/${_id}`, appliance);
+  return data;
+};
+export const deleteAppliance = async (_id: string): Promise<AxiosResponse> => {
+  const result = await axios.delete(`${import.meta.env.VITE_API_URL}/appliance/${_id}`);
+  return result;
+};
