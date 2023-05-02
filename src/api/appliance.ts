fdf862518f8cd8532e829de04ec1f4a88864483a
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-02 21:04:38 +0200

    type never rabbithole

--- src/api/appliance.ts
+++ src/api/appliance.ts
@@ -4,6 +4,7 @@ import axios, { type AxiosResponse } from 'axios';
 
 export const getAppliances = async () : Promise<Appliance[]> => {
   const { data } =  await axios.get<Appliance[]>(`${import.meta.env.VITE_API_URL}/appliance`);
+  console.log('data:', data);
   return data;
 };
 export const getAppliance = async (_id: string): Promise<Appliance> => {
--- src/stores/appliance.ts
+++ src/stores/appliance.ts
@@ -1,20 +1,30 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import { getAppliances } from '@/api/appliance';
+import { getAppliances, deleteAppliance as deleteApplianceAPI } from '@/api/appliance';
 import type Appliance from "../types/Appliance";
+import type { AxiosResponse } from 'axios';
 
 
 export const useApplianceStore = defineStore('appliance', () => {
-  const fetchedAppliances = ref<Appliance[]>();
-  const appliances = computed(() => fetchedAppliances.value);
+  const appliances = ref<Appliance[] | null>([]);
 
-  const setAppliances = async () => {
+  const setAppliances = async (): Promise<void> => {
     try {
-      fetchedAppliances.value = await getAppliances();
-      console.log('fetchAppliances', fetchedAppliances.value);
+      appliances.value = await getAppliances();
+      console.log('applainces:', appliances.value);
+      console.log('fetchAppliances', typeof appliances.value);
     } catch (error) {
       console.error(error);
     }
-  }
-  return { setAppliances, appliances }
+  };
+
+  const deleteAppliance = async (_id: string): Promise<AxiosResponse> => {
+    try {
+      const response = await deleteApplianceAPI(_id);
+      return response;
+    } catch (error) {
+      return <AxiosResponse>error;
+    }
+  };
+  return { setAppliances, deleteAppliance, appliances }
 })
