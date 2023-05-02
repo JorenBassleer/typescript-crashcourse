8ed42693f84da0bd9bf42647eb09692d583fc456
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-02 21:37:58 +0200

    forgot save

--- src/components/AppliancePage.vue
+++ src/components/AppliancePage.vue
@@ -5,28 +5,17 @@
     >
     {{ appliance.name || 'Unknown' }}
     </div>
-    <div
-      v-for="appliance in fetchedAppliances"
-      :key="appliance._id"
-      >
-  
-    </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, type ref } from 'vue';
 import { useApplianceStore } from '../stores/appliance';
 import type Appliance from '@/types/Appliance';
 import { storeToRefs } from 'pinia'
 
 const applianceStore = useApplianceStore();
 
-const { appliances } = storeToRefs(applianceStore);
-console.log('appliances pre for', appliances.value);
-if (appliances.value !== null) {
-  for (const appliance of appliances.value) {
-    console.log('appliance:', appliance._id);
-  }
-}
+const appliances = storeToRefs(applianceStore).appliances as ref<Appliance[]>;
+
 console.log('component appliuances:', appliances.value);
 onMounted(async () => {
   await applianceStore.setAppliances();
