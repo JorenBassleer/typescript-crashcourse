c6ac294e44b5bddc1a0d3fb8d49901b1fd632acc
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-04 19:53:13 +0200

    added files

---
+++ src/components/BrandForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>
+    Brand Form
+  </h2>
+  
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import type Brand from '@/types/Brand';
+
+const newBrand = ref<Brand | null>(null);
+</script>
