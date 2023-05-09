588156c31294edb1acc4cc27a40f232efa0f99ee
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-09 21:35:15 +0200

    added brand form & calls

--- src/components/BrandForm.vue
+++ src/components/BrandForm.vue
@@ -2,11 +2,35 @@
   <h2>
     Brand Form
   </h2>
-  
+  <form
+    v-if="!isSubmitting"
+    @submit="handleFormSubmit"
+  >
+    <input v-model="newBrand.name" />
+  </form>
+  <section v-else>
+    Is submitting...
+  </section>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
-import type Brand from '@/types/Brand';
+import { useBrandStore } from '@/stores/brand';
+import type { BaseBrand } from '@/types/Brand';
 
-const newBrand = ref<Brand | null>(null);
+const newBrand = ref<BaseBrand>({
+  name: '',
+});
+const isSubmitting = ref<Boolean>(false);
+const brandStore = useBrandStore();
+const handleFormSubmit = async () : Promise<void> => {
+  isSubmitting.value = true;
+  try {
+    brandStore.createBrand(newBrand.value);
+    // Notify user
+  } catch (error) {
+    //
+  } finally {
+    isSubmitting.value = false;
+  }
+}
 </script>
