f3b1d3f2098ca3f339e1945700eb1de252bc7ba3
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-12 18:35:01 +0200

    added typeofappliance page

---
+++ src/components/typeOfAppliance/TypeOfAppliancePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <div
+      v-for="typeOfAppliance in typesOfAppliance"
+      :key="typeOfAppliance._id"
+    >
+    {{ typeOfAppliance.name }}
+    </div>
+  </section>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useTypeOfApplianceStore } from '@/stores/typeOfAppliance';
+import { storeToRefs } from 'pinia';
+
+const typeOfApplianceStore = useTypeOfApplianceStore();
+
+const { typesOfAppliance } = storeToRefs(typeOfApplianceStore);
+
+onMounted(async () => {
+  if (typesOfAppliance.value.length === 0) await typeOfApplianceStore.setTypeOfAppliance();
+});
+</script>
