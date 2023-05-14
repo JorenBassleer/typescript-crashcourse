05c29c6d6b115a174767db61eafd51076900b383
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-14 15:11:17 +0200

    added files

---
+++ src/components/appliance/AppliancesPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="background-color: white;">
+    <div
+      v-for="appliance in appliances"
+      :key="appliance._id"
+      style="color: black;"
+    >
+    <router-link :to="{ name: 'single-appliance', params: { id: appliance._id }}"></router-link>{{ appliance.name || 'Unknown' }}
+    </div>
+    <button @click="showForm = !showForm">Create Appliance</button>
+    <ApplianceForm
+      v-if="showForm === true" 
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { useApplianceStore } from '@/stores/appliance';
+import ApplianceForm from './ApplianceForm.vue';
+import RouterLink from 'vue-router';
+import { storeToRefs } from 'pinia'
+
+const applianceStore = useApplianceStore();
+
+const { appliances } = storeToRefs(applianceStore);
+const showForm = ref<Boolean>(false);
+onMounted(async () => {
+  if (appliances.value.length === 0) await applianceStore.setAppliances();
+});
+</script>
