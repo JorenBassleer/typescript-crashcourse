4947e4f66fd0612b2fed35bc4dd391595e53c8fb
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-18 22:32:46 +0200

    added start reservation form

---
+++ src/components/reservation/ReservationForm.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { BaseReservation } from '@/types/Reservation';
+
+const newReservation = ref<BaseReservation>({
+  name: '',
+  details: '',
+  appliances: [],
+  amounts: [],
+  start: null,
+  end: null,
+  user: '', // Get user from state
+});
+</script>
