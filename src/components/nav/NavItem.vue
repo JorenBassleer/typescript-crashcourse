40474a09d73b95fc388ee47d3c6ce3cbde82857b
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-22 20:07:07 +0200

    expanded NavItem.vue

--- src/components/nav/NavItem.vue
+++ src/components/nav/NavItem.vue
@@ -1,11 +1,23 @@
 <template>
   <section>
     <div>
-      Nav item
+      <router-link :to="routerTo">
+        {{ routerText }}
+      </router-link>
     </div>
   </section>
 </template>
 <script setup lang="ts">
 import { defineProps } from 'vue';
-
+interface Props {
+  routerTo: {
+    type: string | Object,
+    required: true,
+  },
+  routerText: {
+    type: string,
+    required: true,
+  },
+}
+defineProps<Props>();
 </script>
