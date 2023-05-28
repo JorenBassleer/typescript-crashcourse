f361e3d7d81b12ca3c93b7537fc8e4d749d2369f
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-05-28 20:37:53 +0200

    login page

--- src/views/app/auth/LoginPage.vue
+++ src/views/app/auth/LoginPage.vue
@@ -1,6 +1,14 @@
 <template>
   Login page
+  <section>
+    <div>
+      Select user type
+    </div>
+
+  </section>
 </template>
 <script setup>
+import { ref } from 'vue';
 
+const user = ref();
 </script>
