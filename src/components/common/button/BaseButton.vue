1eb691654b4cdf24ec77a60a992a10e0db045585
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-02 19:02:19 +0200

    added types to button

--- src/components/common/button/BaseButton.vue
+++ src/components/common/button/BaseButton.vue
@@ -4,5 +4,12 @@
   </div>
 </template>
 <script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
 
+defineProps({
+  type: {
+    type: String as () => 'primary' | 'success' | 'danger' | 'warning',
+    required: true,
+  },
+})
 </script>
