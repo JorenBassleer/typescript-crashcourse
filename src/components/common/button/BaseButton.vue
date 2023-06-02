cb3f6a1f5ced1d9b5e4c029f8292e091b51e0ad2
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-02 19:09:25 +0200

    added size & basic style to BaseButton

--- src/components/common/button/BaseButton.vue
+++ src/components/common/button/BaseButton.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    
+  <div
+  :class="{
+      'bg-green-500': type === 'success',
+      'bg-red-500': type === 'danger',
+      'bg-orange-500': type === 'warning',
+      'bg-slate-600': type === 'primary',
+    }">
+    <slot></slot>
   </div>
 </template>
 <script setup lang="ts">
@@ -9,7 +15,11 @@ import { defineProps, defineEmits } from 'vue';
 defineProps({
   type: {
     type: String as () => 'primary' | 'success' | 'danger' | 'warning',
-    required: true,
+    default: 'primary',
+  },
+  size: {
+    type: String as () => 'mini' | 'small' | 'base' | 'large',
+    default: 'base',
   },
 })
 </script>
