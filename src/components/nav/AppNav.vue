8503ba7dd700cdf61d4d60466e662b7ed9eb6177
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-05 21:14:33 +0200

    converted to navitem

--- src/components/nav/AppNav.vue
+++ src/components/nav/AppNav.vue
@@ -1,12 +1,25 @@
 <template>
   <aside class="h-full bg-secondary p-8">
-    <section class="flex flex-row gap-4 text-gray-900">
-      <div
-        class="drop-shadow-xl border-2 p-2 py-4 rounded-md w-full text-center cursor-pointer hover:drop-shadow-md transition-all duration-300 font-semibold hover:font-normal"
-      >
-        Nav item 1
-      </div>
+    <section class="flex flex-col gap-4 text-gray-900">
+      <NavItem
+        v-for="item in navItems"
+        :key="item.name"
+        class="drop-shadow-2xl border-2 p-2 py-4 rounded-lg w-full text-center cursor-pointer hover:drop-shadow-md transition-all duration-300 font-semibold"
+        :router-text="item.name"
+        :router-to="item.routerTo"
+        router-class="text-gray-700"
+        />
     </section>
+    <!-- Add cart or my current reservation somehwere -->
   </aside>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavItem from './NavItem.vue';
+// Get nav items from external file
+// ++ create type nav item
+const navItems = [
+  { name: 'Dashboard', routerTo:{ path: '/dashboard'}  },
+  { name: 'Appliances', routerTo:{ path: '/appliances'} },
+  { name: 'idk nog iets', routerTo:{ path: '/idk'}  },
+];
+</script>
--- src/components/nav/NavItem.vue
+++ src/components/nav/NavItem.vue
@@ -1,19 +1,22 @@
 <template>
   <section>
     <div>
-      <router-link :to="routerTo" class="text-white">
+      <router-link :to="routerTo" :class="routerClass">
         {{ routerText }}
       </router-link>
     </div>
   </section>
 </template>
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, withDefaults } from 'vue'
 import type { RouteLocationRaw } from 'vue-router'
 
 interface Props {
   routerTo: RouteLocationRaw
   routerText: string
+  routerClass?: string
 }
-defineProps<Props>()
+withDefaults(defineProps<Props>(), {
+  routerClass: 'text-white'
+});
 </script>
