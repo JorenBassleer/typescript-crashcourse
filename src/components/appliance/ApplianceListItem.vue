000a8075a79603a74b220ee961f36d0a137850a6
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-15 22:05:02 +0200

    very small progress on applianceListItem

--- src/components/appliance/ApplianceListItem.vue
+++ src/components/appliance/ApplianceListItem.vue
@@ -1,5 +1,14 @@
 <template>
-  <article v-if="!isLoading">
+  <article
+    v-if="!isLoading"
+    class="flex flex-col w-full h-full p-8 border-2 border-primary hover:border-secondary bg-accent rounded-xl transition-all duration-200"
+  >
+    <h2 class="w-full text-center">
+      {{ appliance?.name }}
+    </h2>
+    <summary>
+      {{ appliance?.details }}
+    </summary>
   </article>
   <section v-else>
     Is loading appliance....
