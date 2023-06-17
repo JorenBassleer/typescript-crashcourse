22217b1b829c25adefe87bf30e0fa434e369009e
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-17 23:36:21 +0200

    added type & brand to appliancelistitem

--- src/components/appliance/ApplianceListItem.vue
+++ src/components/appliance/ApplianceListItem.vue
@@ -1,36 +1,34 @@
 <template>
-  <article
-    v-if="!isLoading"
-    class="flex flex-col w-full h-full p-8 border-2 border-primary hover:border-secondary bg-accent rounded-xl transition-all duration-200 text-gray-700"
-  >
+  <article class="flex flex-col w-full h-full p-8 border-2 border-primary hover:border-secondary bg-accent rounded-xl transition-all duration-200 text-gray-700">
     <h2 class="w-full text-center text-2xl">
       {{ appliance?.name }}
     </h2>
+    <section class="w-full flex flex-row items-center">
+      <div class="w-1/2">
+        {{ foundBrand?.name  || 'No brand found'}}
+      </div>
+      <div class="w-1/2">
+        {{ foundTypeOfAppliance?.name || 'No type found' }}
+      </div>
+    </section>
     <summary>
       {{ appliance?.details }}
     </summary>
   </article>
-  <section v-else>
-    Is loading appliance....
-  </section>
 </template>
 <script setup lang="ts">
-import { withDefaults, defineProps, computed } from 'vue';
+import { defineProps, computed } from 'vue';
 import type { ApplianceRecord } from '@/types/Appliance';
 import type { BrandRecord } from '@/types/Brand';
 import type { TypeOfApplianceRecord } from '@/types/TypeOfAppliance';
 
 interface Props {
-  appliance?: ApplianceRecord | null;
+  appliance: ApplianceRecord;
   brands: BrandRecord[];
   typesOfAppliance: TypeOfApplianceRecord[]
 }
-const props = withDefaults(defineProps<Props>(), {
-  appliance: null,
-});
+const props = defineProps<Props>();
 
-const isLoading = computed<Boolean>(() => {
-  return props.appliance === null;
-});
-
+const foundBrand = computed(() => props.brands.find((brand) => brand._id === props.appliance.brand));
+const foundTypeOfAppliance = computed(() => props.typesOfAppliance.find((type) => type._id === props.appliance.type));
 </script>
