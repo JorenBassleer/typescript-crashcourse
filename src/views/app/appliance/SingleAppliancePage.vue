46942e28d2067de63201002ffc4be2c3ea941b08
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-19 20:51:35 +0200

    single page logic

--- src/views/app/appliance/SingleAppliancePage.vue
+++ src/views/app/appliance/SingleAppliancePage.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>single-appliance</div>
+  <section v-if="currentAppliance?._id">
+    Current Appliance: {{ currentAppliance.name }}    
+  </section>
+  <section v-else>
+    We could not find the appliance you're looking for
+  </section>
 </template>
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { onBeforeMount, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { useApplianceStore } from '@/stores/appliance';
+import { storeToRefs } from 'pinia';
+import type { ApplianceRecord } from '@/types/Appliance';
+
+const applianceStore = useApplianceStore();
+
+const { appliances } = storeToRefs(applianceStore);
+
+const route = useRoute();
+
+const currentAppliance = computed<ApplianceRecord | null>(() => {
+  const foundAppliance = appliances.value.find((appliance) => appliance._id === route.params.id)
+  if (!foundAppliance) return null;
+  return foundAppliance;
+});
+
+onBeforeMount(() => {
+  if (appliances.value.length === 0) applianceStore.setAppliances();
+});
 </script>
