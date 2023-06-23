f52f1c56f82a393ef8d425b334747d07f5b5d586
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-23 20:44:19 +0200

    tiny progress

--- src/models/Appliance.ts
+++ src/models/Appliance.ts
@@ -2,5 +2,10 @@ import { ApplianceRecord } from '@/types/Appliance'
 
 class Appliance extends ApplianceRecord {
   
-  constructor(_id: string, brand: string, type: string, store
+  constructor(_id: string, brand: string, type: string, store) {
+    this.id = _id
+    this.brand = brand
+    this.type = type
+    this.store = store
+  }
 }
