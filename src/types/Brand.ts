bf3e8ff8c97c0f213d992846dc093ec8dfd38a6f
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-06-25 19:06:25 +0200

    expanded brand

--- src/types/Brand.ts
+++ src/types/Brand.ts
@@ -4,3 +4,22 @@ export interface BaseBrand {
   name: string
 }
 export interface BrandRecord extends BaseBrand, BaseRecord {}
+
+export class Brand implements BrandRecord {
+  readonly _id: string
+  name: string
+  readonly createdAt: Date
+  readonly updatedAt: Date
+
+  constructor(
+    _id: string,
+    createdAt: Date,
+    updatedAt: Date,
+    name: string,
+  ) {
+    this._id = _id;
+    this.name = name;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
