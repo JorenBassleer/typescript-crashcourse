a7bdfb4a3abfbf3aa352e0ccf41ed8d20d3c57c6
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-07-03 19:26:10 +0200

    added reservation class

--- src/types/Reservation.ts
+++ src/types/Reservation.ts
@@ -3,13 +3,47 @@ import type BaseRecord from './Record';
 export interface BaseReservation {
   name: string;
   details?: string;
-  start: Date;
-  end: Date;
-  user: '';
+  start: Date | null;
+  end: Date | null;
+  user: string;
   appliances: string[];
   amounts: number[];
-  createdAt: Date;
-  updatedAt: Date;
 }
 
-export interface ReservationRecord extends BaseRecord, BaseReservation {}
+export interface ReservationRecord extends BaseRecord, BaseReservation { }
+
+export class Reservation implements ReservationRecord {
+  readonly _id: string;
+  name: string;  
+  details?: string;
+  start: Date | null;
+  end: Date | null;
+  user: string;
+  appliances: string[];
+  amounts: number[];
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+  constructor(
+    _id: string,
+    start: Date,
+    end: Date,
+    name: string,
+    details: string,
+    appliances: string[],
+    amounts: number[],
+    user: string,
+    createdAt: Date,
+    updatedAt: Date,
+  ) {
+    this._id = _id;
+    this.user = user;
+    this.name = name;
+    this.details = details;
+    this.appliances = appliances;
+    this.start = start;
+    this.end = end;
+    this.amounts = amounts;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+};
