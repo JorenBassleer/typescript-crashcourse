57f0d13fe99f86c68d92b8f9c55cc3c595b59212
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-09-23 16:28:11 +0200

    fixed class conditionals

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <section>
     <header></header>
-    <main :class="{ 'flex w-full h-screen': !auth.currentUser }">
-      <TheNavBar :class="{ 'lg:w-1/6 rounded md:w-2/6 sm:w-3/5': !auth.currentUser }" />
-      <section :class="{ 'w-5/6 p-8': !auth.currentUser }">
+    <main :class="{ 'flex w-full h-screen': auth.currentUser }">
+      <TheNavBar :class="{ 'lg:w-1/6 rounded md:w-2/6 sm:w-3/5': auth.currentUser }" />
+      <section :class="{ 'w-5/6 p-8': auth.currentUser }">
         <RouterView />
       </section>
     </main>
