dc4b5ce088f1a6aa884711896bb9ee9b5a7cf791
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-09-27 20:41:17 +0200

    stash

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,46 +1,44 @@
 <template>
-  <FormLayout title="Login">
-    <form
-      class="p-6 w-full flex flex-col gap-2 px-10"
-      @submit.prevent="handleLogin"
-    >
-      <div class="flex gap-2">
-        Select user type
-        <select placeholder="Select">
-          <option>Admin</option>
-          <option>User</option>
-        </select>
-      </div>
-      <div class="flex flex-col">
-        <label>Email</label>
-        <BaseInput
-          v-model="userLogin.email"
-          type="text"
-          placeholder="Enter email"
-          sub-text="e.g. user@email.com"
-        />
-      </div>
-      <div class="flex flex-col">
-        <label>Password</label>
-        <BaseInput
-          v-model="userLogin.password"
-          type="password"
-          placeholder="********"
-        />
-      </div>
-      <div class="flex justify-end items-center gap-6">
-        <BaseButton @click="$router.push({ name: 'register' })">
-          Register
-        </BaseButton>
-        <BaseButton
-          class="my-2"
-          @click="handleLogin"
-        >
-          Login
-        </BaseButton>
-      </div>
-    </form>
-  </FormLayout>
+  <form
+    class="p-6 w-full flex flex-col gap-2 px-10"
+    @submit.prevent="handleLogin"
+  >
+    <div class="flex gap-2">
+      Select user type
+      <select placeholder="Select">
+        <option>Admin</option>
+        <option>User</option>
+      </select>
+    </div>
+    <div class="flex flex-col">
+      <label>Email</label>
+      <BaseInput
+        v-model="userLogin.email"
+        type="text"
+        placeholder="Enter email"
+        sub-text="e.g. user@email.com"
+      />
+    </div>
+    <div class="flex flex-col">
+      <label>Password</label>
+      <BaseInput
+        v-model="userLogin.password"
+        type="password"
+        placeholder="********"
+      />
+    </div>
+    <div class="flex justify-end items-center gap-6">
+      <BaseButton @click="$router.push({ name: 'register' })">
+        Register
+      </BaseButton>
+      <BaseButton
+        class="my-2"
+        @click="handleLogin"
+      >
+        Login
+      </BaseButton>
+    </div>
+  </form>
 </template>
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
@@ -49,7 +47,6 @@ import { storeToRefs } from 'pinia';
 import { useRouter } from 'vue-router';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import type { UserAuth } from '@/types/User';
-import FormLayout from '@/components/form/FormLayout.vue';
 
 const userLogin = ref<UserAuth>({
   email: '',
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,47 +1,44 @@
 <template>
-  <FormLayout title="Register">
-    <el-form
-      class="p-6 w-full flex flex-col gap-2 px-10"
-      @submit.prevent="handleRegister"
-    >
-      <div class="flex flex-col">
-        <label>Email</label>
-        <BaseInput
-          v-model="newUser.email"
-          type="text"
-          placeholder="Enter email"
-          sub-text="e.g. user@email.com"
-        />
-      </div>
-      <div class="flex flex-col">
-        <label>Password</label>
-        <BaseInput
-          v-model="newUser.password"
-          type="password"
-          placeholder="*******"
-        />
-      </div>
-      <div class="flex justify-end items-center gap-6">
-        <BaseButton @click="handleRegister">
-          Register
-        </BaseButton>
-        <BaseButton>Register with Google</BaseButton>
-      </div>
-    </el-form>
-  </FormLayout>
+  <el-form
+    class="p-6 w-full flex flex-col gap-2 px-10"
+    @submit.prevent="handleRegister"
+  >
+    <div class="flex flex-col">
+      <label>Email</label>
+      <BaseInput
+        v-model="newUser.email"
+        type="text"
+        placeholder="Enter email"
+        sub-text="e.g. user@email.com"
+      />
+    </div>
+    <div class="flex flex-col">
+      <label>Password</label>
+      <BaseInput
+        v-model="newUser.password"
+        type="password"
+        placeholder="*******"
+      />
+    </div>
+    <div class="flex justify-end items-center gap-6">
+      <el-button @click="handleRegister">
+        Register
+      </el-button>
+      <el-button>Register with Google</el-button>
+    </div>
+  </el-form>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { UserAuth } from '@/types/User';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useRouter } from 'vue-router';
-import FormLayout from '@/components/form/FormLayout.vue';
 
 const router = useRouter();
 
 const newUser = ref<UserAuth>({
   email: '',
-  password: '',
+  password: ''
 });
 const handleRegister = async (): Promise<void> => {
   try {
