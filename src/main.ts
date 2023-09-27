517223787417a7947c6b1e3ce0fb930bccee68d1
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-09-27 20:10:39 +0200

    added element plus

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import './assets/main.css';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import BaseButton from './components/common/button/BaseButton.vue';
 import BaseInput from './components/common/input/BaseInput.vue';
 import { initializeApp } from 'firebase/app';
@@ -16,4 +18,5 @@ createApp(App)
   .use(router)
   .component('BaseButton', BaseButton)
   .component('BaseInput', BaseInput)
+  .use(ElementPlus)
   .mount('#app');
